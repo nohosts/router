6d627ffc1c06587c9edca18e22440b0e691894de
avwo <avwu@qq.com>
2020-06-20 00:19:46 +0800

    refactor: refine code

---
+++ test/proxy.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const Router = require('../lib');
+
+const {
+  SPACE_NAME,
+  GROUP_NAME,
+  ENV_NAME,
+  NOHOST_RULE,
+  NOHOST_VALUE,
+} = Router;
+const servers = [
+  {
+    host: '127.0.0.1',
+    port: 8080,
+  },
+  {
+    host: '127.0.0.1',
+    port: 8080,
+  },
+];
+
+const router = new Router(servers);
+
+const addEnv = (req, res) => {
+  res.on('error', () => {});
+  const { headers } = req;
+  // 设置规则
+  headers[NOHOST_RULE] = encodeURIComponent('ke.qq.com file://{test.html}');
+  headers[NOHOST_VALUE] = encodeURIComponent(JSON.stringify({ 'test.html': 'hell world.' }));
+  // 设置环境
+  headers[SPACE_NAME] = encodeURIComponent('imweb');
+  headers[GROUP_NAME] = encodeURIComponent('avenwu');
+  headers[ENV_NAME] = encodeURIComponent('测试'); // 可选
+};
+
+const server = http.createServer(async (req, res) => {
+  await addEnv(req, res);
+  router.proxy(req, res);
+  // 如果需要修改响应内容，可以采用下面的方式
+  // const svrRes = await router.proxy(req);
+  // res.writeHead(svrRes.statusCode, svrRes.headers);
+  // svrRes.pipe(res);
+});
+// TCP 请求
+server.on('connect', async (req, socket) => {
+  await addEnv(req, socket);
+  router.proxy(req, socket);
+});
+// WebSocket 请求
+server.on('upgrade', async (req, socket) => {
+  await addEnv(req, socket);
+  router.proxy(req, socket);
+});
+
+server.listen(5566);
